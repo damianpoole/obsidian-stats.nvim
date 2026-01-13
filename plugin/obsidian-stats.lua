ee1d09caad7b99b1d5cd3298d10c86eb276e467b
Damian Poole <damian.poole@gmail.com>
2026-01-13 21:44:20 +0000

    apply formatting

--- plugin/obsidian-stats.lua
+++ plugin/obsidian-stats.lua
@@ -1,10 +1,10 @@
 -- Prevent loading plugin twice
 if vim.g.loaded_obsidian_stats == 1 then
-  return
+	return
 end
 vim.g.loaded_obsidian_stats = 1
 
 -- Create user command
 vim.api.nvim_create_user_command("ObsidianStats", function()
-  require("obsidian-stats").show_stats()
+	require("obsidian-stats").show_stats()
 end, {})
